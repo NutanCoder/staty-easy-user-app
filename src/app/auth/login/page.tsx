1de4652dac6b18af3c96b6d58424f3b3f7d94085
Nutan Singh <nutansingh159@gmail.com>
2025-07-10 20:52:02 +0530

    setup basic setructure

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,11 +2,11 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Homepage",
+  title: "Login Page | Stay Easy",
 };
 
-function HomePage() {
-  return <div>HomePage</div>;
+function Page() {
+  return <div>Page</div>;
 }
 
-export default HomePage;
+export default Page;
